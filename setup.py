2e75ef451b32dbabbd887f682c6ebafcad69fdf1
tswiger34 <tswiger34@gmail.com>
2025-02-04 21:17:13 -0700

    finish setup file, add license

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="saftdatamgmt",
+    version="0.1.0",
+    description="A package for managing financial data, specifically made for algorithmic traders",
+    author="Travis Swiger",
+    author_email="tswiger@stoneagefinancialtechnology.com",
+    url="https://github.com/S-A-F-T-Organization/DataManagement",
+    packages=find_packages(),
+    install_requires=[
+        "SQLAlchemy>=1.4",
+        "PyYAML"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
